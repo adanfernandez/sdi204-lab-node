25160e0d4b02e041170d9159cbbecaf90419315c
adanvetusta <adanvetusta@gmail.com>
2019-03-20 10:18:50 +0100

    SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas.

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-// Módulos
 var express = require('express');
 var app = express();
+var swig = require('swig');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
 
-
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
-// lanzar el servidor
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
+});
+
+app.post("/cancion", function(req, res) {
+    res.send("Canción agregada:"+req.body.nombre +"<br>"
+        +" genero :" +req.body.genero +"<br>"
+        +" precio: "+req.body.precio);
+});
+
+app.get('/promo*', function (req, res) {
+    res.send('Respuesta patrón promo* ');
 });
