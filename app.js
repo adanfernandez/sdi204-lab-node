6d5f794bc5a5e441d42351513e757ffec890bdd5
adanvetusta <adanvetusta@gmail.com>
2019-03-20 09:26:27 +0100

    Revert "SDI-NodeJS- 6,7 División de las rutas en módulos, peticiones Get y parámetros"
    This reverts commit 5f9f6658fc2912e772cfdca7096764642ac67fe1.

--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
 // Variables
 app.set('port', 8081);
 
@@ -21,14 +15,4 @@ require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 // lanzar el servidor
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
-});
-
-app.post("/cancion", function(req, res) {
-    res.send("Canción agregada:"+req.body.nombre +"<br>"
-        +" genero :" +req.body.genero +"<br>"
-        +" precio: "+req.body.precio);
-});
-
-app.get('/promo*', function (req, res) {
-    res.send('Respuesta patrón promo* ');
 });
