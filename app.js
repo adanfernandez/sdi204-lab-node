a0c49d29407857e9682ec76242c92596b9c744d5
adanvetusta <adanvetusta@gmail.com>
2019-03-26 23:41:45 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+
 var mongo = require('mongodb');
 
 var swig = require('swig');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 
 app.use(express.static('public'));
 
@@ -17,8 +22,8 @@ app.set('db','mongodb://admin:sdi123456@tiendamusica-shard-00-00-ozuku.mongodb.n
 
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
